9a558fb2cd4fa0d5d2974dcce2e3238a03110c3c
CrispyCrunchy <viktor_kraft@hotmail.com>
2021-03-10 13:05:50 +0100

    undoredo

--- dragndrop.js
+++ dragndrop.js
@@ -1,194 +1,3 @@
-<<<<<<< HEAD
-var table1 =
-    {
-        "cart" :
-            [
-
-            ],
-
-        "amount" :
-            [
-
-            ]
-    };
-
-var table2 =
-    {
-        "cart" :
-            [
-
-            ],
-
-        "amount" :
-            [
-
-            ]
-    };
-
-var table3 =
-    {
-        "cart" :
-            [
-
-            ],
-
-        "amount" :
-            [
-
-            ]
-    };
-
-var table4 =
-    {
-        "cart" :
-            [
-
-            ],
-
-        "amount" :
-            {
-
-            }
-    };
-
-var tables = {
-    "table1": table1,
-    "table2": table2,
-    "table3": table3,
-    "table4": table4
-};
-
-function allowDrop(allowdropevent) {
-    allowdropevent.preventDefault();
-}
-
-function drag(dragevent) {
-    dragevent.dataTransfer.setData("text", $("#" + dragevent.target.id).data("item"));
-}
-
-function createOrderTable = (table, dropevent) => {
-    const previousTable = table.cart;
-
-    return {
-        execute() {
-            if ((namn in table1.amount)){
-                table1.amount[namn] +=1;
-            }else{
-                table1.cart.push(beverage);
-                table1.amount[namn] = 1;
-            }
-        },
-
-        undo() {
-            table.cart = previousTable1;
-        }
-    }
-}
-
-function drop(dropevent) {
-    dropevent.preventDefault();
-
-    var id = dropevent.target.id;
-
-    let beverage = JSON.parse(dropevent.dataTransfer.getData("text"));
-    // console.log(beverage.namn);
-    // console.log(typeof(beverage));
-    namn = beverage.namn
-    switch(id){
-        case "table1":
-            console.log("1");
-            createCommandManager(tables).doCommand(createOrderTable(table1, dropevent));
-            break;
-
-        case "table2":
-            createCommandManager(tables).doCommand(createOrderTable(table2, dropevent));
-            break;
-
-        case "table3":
-            createCommandManager(tables).doCommand(createOrderTable(table3, dropevent));
-            break;
-
-        case "table4":
-            createCommandManager(tables).doCommand(createOrderTable(table4, dropevent));
-            break;
-
-        default:
-            break;
-    }
-}
-
-const TABLEORDER = "TABLEORDER"
-
-const commands = (target, tool) => {
-    [TABLEORDER]: createOrderTable(target, tool)
-}
-
-const createCommandManager = (target) => {
-    let history = [null];
-    let position = 0;
-
-    return {
-        doCommand(commandType) {
-            if (position < history.length -1) {
-                history = history.slice(0, position + 1)
-            }
-
-            if (commands[commandType]) {
-                const concreteCommand = commands[commandType](target);
-                history.push(concreteCommand);
-                position += 1;
-
-                concreteCommand.execute();
-            }
-        },
-
-        undo() {
-            if (position > 0) {
-                history[position].undo();
-                position -= 1;
-            }
-        },
-
-        redo() {
-            if(position < history.length -1) {
-                position += 1;
-                history[position].execute();
-            }
-        }
-    }
-}
-
-/*
-window.onload = beverageList;
-function beverageList() {
-    var listDiv = document.getElementById('list');
-    var ul=document.createElement('ul');
-
-    listDiv.appendChild(ul);
-
-    //List items
-    for (var i = 0; i < Object.keys(DB1.sprits).length; ++i) {
-
-        var li= document.createElement('li');
-        li.setAttribute("ondragstart", "drag(event)");
-        li.setAttribute("draggable", "true");
-        li.setAttribute("id", i);
-
-        //List properties of item
-        var innerUl = document.createElement('ul');
-        var name = document.createElement('li');
-        var price = document.createElement('li');
-        name.append(DB1.sprits[i].namn);
-        price.append(DB1.sprits[i].prisinklmoms);
-        innerUl.appendChild(name);
-        innerUl.appendChild(price);
-        li.appendChild(innerUl);
-        ul.appendChild(li);
-        $("#" + i).data("item", JSON.stringify(DB1.sprits[i]));
-    }
-}
-*/
-=======
 var table1 =
     {
         "cart" :
@@ -259,6 +68,9 @@ function drag(dragevent) {
 const createOrderTable = (table, dropevent) => {
 
     const previousTable = table.cart;
+    dropevent.preventDefault();
+
+    var id = dropevent.target.id;
 
 
     return {
@@ -324,12 +136,10 @@ function drop(dropevent) {
     }
 }
 
-const INCREMENT = "INCREMENT"
-const DECREMENT = "DECREMENT"
+const TABLEORDER = "TABLEORDER"
 
-const commands = {
-    [INCREMENT]: createIncrementCommand,
-    [DECREMENT]: createDecrementCommand
+const commands = (target, tool) => {
+    [TABLEORDER]: createOrderTable(target, tool)
 }
 
 const createCommandManager = (target) => {
@@ -366,5 +176,3 @@ const createCommandManager = (target) => {
         }
     }
 }
-
->>>>>>> 346a86cc767f55eaa5fc3e6dc7590610200d0deb
